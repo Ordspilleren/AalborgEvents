63f21cc5939bfbc083c9a9b0151dc7e5fb5b5a5f
Lasse Bjerre <lasse@lgb.dk>
2015-04-27 15:12:58 +0200

    Opdaterede eventside med kort

--- eventside.php
+++ eventside.php
@@ -5,19 +5,24 @@
 		<div class="col-md-4">
 			<div class="event-info">
 				<img src="http://www.claussondberg.dk/wp-content/uploads/25-FAELS-Karneval-18.jpg" class="img-responsive" alt="">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, porro iure doloribus vitae nisi qui quo natus velit alias asperiores, officia vero accusamus explicabo ullam. Ratione aut atque excepturi praesentium.</p>
-				<button type="button" class="btn btn-info btn-block">Køb billet</button>
+				<address>
+					<strong>Studenterhuset</strong><br>
+					Gl. Torv 10<br>
+					9000 Aalborg
+				</address>
+				<iframe width="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=Studenterhuset%2C%20Gammeltorv%2C%20Aalborg%2C%20Danmark&key=AIzaSyAPR6aYdpgiTTUkNn0qIS8vG0mTUBkDszs"></iframe>
+				<button type="button" class="btn btn-info btn-block" disabled="disabled">Køb billet</button>
 			</div>
 		</div>
-		<div class="col-md-5">
+		<div class="col-md-4">
 			<div class="event-desc">
 				<h1>Dizzy Mizz Lizzy</h1>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam itaque ullam illum possimus blanditiis soluta beatae culpa dolorem libero, ipsum fuga cupiditate nulla, voluptatibus voluptate quisquam ipsam reprehenderit aliquam! Quo.</p>
 			</div>
 		</div>
-		<div class="col-md-3">
+		<div class="col-md-4">
 			<div class="featured-events">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam autem soluta beatae nihil saepe accusantium totam eligendi doloremque voluptatibus corrupti, consequuntur neque dolor officia, fuga perspiciatis commodi! Illum, veniam, aspernatur?
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus a blanditiis tempora nobis! Quaerat non, ullam voluptates delectus sunt earum eaque et, mollitia iusto similique illo temporibus deleniti cum nesciunt.
 			</div>
 		</div>
 	</div>
