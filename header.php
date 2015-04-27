e756f06cc18ede10d9e0ab5668b65eb614062bfd
mthorup <mthoru14@student.aau.dk>
2015-04-27 13:07:18 +0200

    Tilføjet mine arrangementer og scale ved hover

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Aalborg Events</title>
 	<link rel="stylesheet" href="css/bootstrap.css">
---
+++ minside.php
@@ -0,0 +1,52 @@
+<? include('header.php'); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-7">
+            <h2>Mine arrangmenter</h2>
+            <p>Dette er din personlige side hvor du kan se de arrangementer du er tilmeldt til.</p>
+            <ul class="event-list">
+		    <li>
+			<img alt="Independence Day" src="http://www.claussondberg.dk/wp-content/uploads/25-FAELS-Karneval-18.jpg" />
+			<div class="info">
+			    <span class="date">24/05/2015</span>
+			    <h2 class="title">Aalborg Karneval</h2>
+			    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto ipsum, earum facilis dignissimos numquam reiciendis omnis sit voluptas, sint.</p>
+			</div>
+			<div class="social">
+			    <ul>
+				<li class="facebook" style="width:33%;"><a href="#facebook"><span class="fa fa-facebook"></span></a></li>
+				<li class="twitter" style="width:34%;"><a href="#twitter"><span class="fa fa-twitter"></span></a></li>
+				<li class="google-plus" style="width:33%;"><a href="#google-plus"><span class="fa fa-google-plus"></span></a></li>
+			    </ul>
+			</div>
+		    </li>
+                    <li>
+			<img alt="Independence Day" src="http://i2-images1.tv2net.dk/s/21/20166321-b09a8711b8fc517b1c8b2401bc638335.jpeg" />
+			<div class="info">
+                            <span class="date">27/05/2015</span>
+			    <h2 class="title">Grill v/ Østre Anlæg</h2>
+			    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto ipsum, earum facilis dignissimos numquam reiciendis omnis sit voluptas, sint.</p>
+			</div>
+			<div class="social">
+			    <ul>
+				<li class="facebook" style="width:33%;"><a href="#facebook"><span class="fa fa-facebook"></span></a></li>
+				<li class="twitter" style="width:34%;"><a href="#twitter"><span class="fa fa-twitter"></span></a></li>
+				<li class="google-plus" style="width:33%;"><a href="#google-plus"><span class="fa fa-google-plus"></span></a></li>
+			    </ul>
+			</div>
+		    </li>
+		</ul>   
+        </div>
+        <div class="col-md-5">
+            <h2>Populære arrangementer</h2>
+            <p>Lorem ipsum dolar sit amet, consectetur adipisi</p>
+            <ul class="popular-event">
+                <li>
+                    <h2>Hej</h2>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<? include('footer.php'); ?>
