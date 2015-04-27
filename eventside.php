9368e4dfa0fb4cb090e74967d27e6e26504b7064
Lasse Bjerre <lasse@lgb.dk>
2015-04-27 11:53:24 +0200

    Små ændringer i eventliste + tilføjede event side

---
+++ eventside.php
@@ -0,0 +1,25 @@
+<? include('header.php'); ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<div class="event-info">
+				<img src="http://www.claussondberg.dk/wp-content/uploads/25-FAELS-Karneval-18.jpg" class="img-responsive" alt="">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, porro iure doloribus vitae nisi qui quo natus velit alias asperiores, officia vero accusamus explicabo ullam. Ratione aut atque excepturi praesentium.
+			</div>
+		</div>
+		<div class="col-md-5">
+			<div class="event-desc">
+				<h1>Dizzy Mizz Lizzy</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam itaque ullam illum possimus blanditiis soluta beatae culpa dolorem libero, ipsum fuga cupiditate nulla, voluptatibus voluptate quisquam ipsam reprehenderit aliquam! Quo.</p>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="featured-events">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam autem soluta beatae nihil saepe accusantium totam eligendi doloremque voluptatibus corrupti, consequuntur neque dolor officia, fuga perspiciatis commodi! Illum, veniam, aspernatur?
+			</div>
+		</div>
+	</div>
+</div>
+
+<? include('footer.php'); ?>
