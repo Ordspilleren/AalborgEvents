66ae498576a4c6e59072632f3d11411aa56a78a9
sbpo <simonpoulsen@Simons-MacBook-Pro.local>
2015-04-30 14:07:30 +0200

    lavet lidt mere på form

--- opret-arrangement.php
+++ opret-arrangement.php
@@ -7,27 +7,117 @@ include ('header.php'); ?>
 			<div class="col-sm-12">
 				<div class="panel panel-default">
 					<div class="panel-heading">
-						<h3>Opret et nyt arrangement</h3>
+							<h3>Opret et nyt arrangement</h3>
 					</div>
 					<div class="panel-body">
 						<div>
 							<p>Her kan du oprette et arrangement på siden. Arrangementet vil være synligt for alle efter mellem 20-24 timer.</p>
+							<br>
 						</div>
-						<form action="eventsubmit.php" method="post" id="form">
+						
+						<form role="form" action="eventsubmit.php" method="post" id="form">
 							<div class="row">
+								
+								<!-- Billede upload (lav preview senere) -->
+								<div class="form-group col-sm-6">
+									<div id="image-preview" style="display: block">
+										<img id="preview-img" class="img-responsive img-thumbnail" src="img/opretthumbnail.jpeg">
+									</div>
+									<input type="file" name="billede" id="billede">
+								</div>
+								
+								<!-- Navn på arrangementet -->
 								<div class="col-sm-6">
 									<div class="form-group">
-										<label for="1"></label>
-										<input type="text" name="fornavn" id="fornavn" class="form-control input-sm" placeholder="Indtast dit fornavn" required>
+										<label for="eventnavn">Navn på event:</label>
+										<input type="text" name="eventnavn" id="fornavn" class="form-control input-sm" placeholder="Indtast navnet på arrangementet" required>
 									</div>
 								</div>
+								
+
+								<!-- Navn på arrangør af arrangement -->
 								<div class="col-sm-6">
 									<div class="form-group">
-										<input type="text" name="efternavn" id="efternavn" class="form-control input-sm" placeholder="Indtast dit efternavn" required>
+											<label for="efternavn">Arrangør:</label>
+										<div>
+											<input type="text" name="efternavn" id="efternavn" class="form-control input-sm" placeholder="Indtast sted" required>
+										</div>
 									</div>
 								</div>
+
+								<!-- dato vælger -->
+								<div class="col-sm-6">
+									<div class="form-group">
+											<label for="dato">Vælg en dato og starttidspunkt:</label>
+										<div class="form-inline">
+										<?php
+										//sæt dato idag til min dato brugeren kan indtaste. 
+										$datoidag = date('Y-m-d');
+										?>
+
+										<span class="inner-addon">
+											<i class="glyphicon glyphicon-calendar"></i>
+											<input type="date" name="startdato" id="dato" class="form-control input-sm" min="<?php echo $datoidag; ?>" max="2020-12-31" required>
+										</span>
+											<!-- valgfrit om man vil tilhøje en tid ??-->
+										
+										<span class="inner-addon">
+											<i class="glyphicon glyphicon-time"></i>
+											<input type="time" name="starttid" id="starttid" class="form-control input-sm">
+										</span>
+
+											<button class="btn btn-info btn-xs" id="sluttid">Tilføj sluttidspunkt</button>
+										</div>
+									</div>
+								</div>
+							
+								
+								<!-- Hvis slutdato skal vælges - er gemt som standart -->
+								<div class="col-sm-6" id="slutdato" style="display: none">
+									<div class="form-group">
+											<label for="dato">Vælg slutdato og sluttidspunkt:</label>
+										<div class="form-inline">
+							
+											<span class="inner-addon">
+												<i class="glyphicon glyphicon-calendar"></i>
+												<input type="date" name="slutdato" id="dato2" class="form-control input-sm" min="<?php echo $datoidag; ?>" max="2020-12-31" required>
+											</span>
+
+											<span class="inner-addon">
+												<i class="glyphicon glyphicon-time"></i>
+												<input type="time" name="sluttid" id="tid2" class="form-control input-sm">
+											</span>
+										
+										</div>
+									</div>
+								</div>
+								
+								<!-- addresse på arrangementet -->
+								<div class="col-sm-6">
+									<div class="form-group">
+										<label for="adresse">Indtast adresse hvor arrangementet afholdes:</label>
+										<div class="">
+											<input type="text" name="adresse" class="form-control input-sm" placeholder="Indtast et vejnavn og nummer">
+										</div>
+									</div>
+								</div>
+
+
+								<!-- Kategorier (udskift evt. med keywordS?? lave et eller andet her.) -->
+								<div class="col-sm-6">
+									<div class="form-group">
+										<label for="kategori">Vælg en eller flere kategorier:</label>
+										<div class="">
+											<input type="dropdown" name="kategorier" class="form-control input-sm">
+										</div>
+									</div>
+								</div>
+
+
+
 							</div>
 
+							<!--  -->
 								<div class="form-group">
 									<input type="email" name="email" id="email" class="form-control input-sm" placeholder="Indtast din email" required>
 								</div>
@@ -50,6 +140,13 @@ include ('header.php'); ?>
 			</div>
 		</div>
 	</div>
-
+<script>
+window.onload = function() {
+$( "#sluttid" ).click(function() {
+  $( "#slutdato" ).show( "slow" );
+  $( "#sluttid" ).hide();
+});
+};
+</script>
 
 <? include('footer.php'); ?>
