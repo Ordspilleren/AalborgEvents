63e75ecf2332ec864fb81788dac705094f473f14
Rasmus T. Larsen <r0ede1991@gmail.com>
2015-05-11 10:55:38 +0200

    footer!
    lol

--- footer.php
+++ footer.php
@@ -1,46 +1,36 @@
 	<footer class="side-footer">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-3">
+				<div class="col-md-4">
 					<ul>
-						<li>Dette er et link</li>
-						<li>Et andet link</li>
-						<li>Flere links</li>
-						<li>Flere links</li>
+						<li><b>Kontakt</b></li>
+						<li>Badehus vej 9</li>
+						<li>9000 Aalborg</li>
+						<li>Tlf: 12345678</li>
+						<li>E-mail: kontakt@eventit.dk</li>
 					</ul>
 				</div>
 
-				<div class="col-md-3">
+				<div class="col-md-4">
 					<ul>
-						<li>Dette er et link</li>
-						<li>Et andet link</li>
-						<li>Flere links</li>
-						<li>Flere links</li>
+						<li><b>Hjælp</b></li>
+						<li>For telefon support: 87654321</li>
+						<li>For supportchat "Klick her"</li>
 					</ul>
 				</div>
 
-				<div class="col-md-3">
+				<div class="col-md-4">
 					<ul>
-						<li>Dette er et link</li>
-						<li>Et andet link</li>
-						<li>Flere links</li>
-						<li>Flere links</li>
-					</ul>
-				</div>
-
-				<div class="col-md-3">
-					<ul>
-						<li>Dette er et link</li>
-						<li>Et andet link</li>
-						<li>Flere links</li>
-						<li>Flere links</li>
+						<li><b>Om os</b></li>
+						<li>Eventit.dk er ejet af Eventit A/S og blev lanceret i 2015, med henblik på at
+						skabe overblik over hvad der sker i Aalborg by.</li>
 					</ul>
 				</div>
 			</div>
 
 			<div class="row">
 				<div class="copyright">
-					COPYRIGHT HAROLD 2015
+					COPYRIGHT EVENTIT A/S 2015
 				</div>
 			</div>
 		</div>
